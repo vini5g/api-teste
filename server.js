11c68f5bbbc1cb242d50b9cf466705537d7488cc
vinicius-santos-programmer <vinicius.a.moreira.santos@gmail.com>
2021-01-22 10:51:05 -0300

    esqueleto base do projeto

--- server.js
+++ server.js
@@ -1,7 +1,4 @@
-const express = require('express');
-const cors = require('cors');
-
-const server = express();
+const server = require('./src/app');
 
 server.listen(3333, () => {
     console.log('Servidor tá on na porta 3333');
